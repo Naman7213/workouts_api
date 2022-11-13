57bae2352fb0fbac87195e0d039a8b93be43f8ba
Naman7213 <72452798+Naman7213@users.noreply.github.com>
2022-11-13 18:51:44 +0530

    cors added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 const exercise_routes = require("./api/routes");
 
 app.use("/exercises", exercise_routes);
